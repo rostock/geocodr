02dcecfddc899f27e34cc2110d646a0034cda1ba
Oliver Tonnhofer <olt@bogosoft.com>
2018-05-30 13:16:51 +0200

    build: bump version

--- api/setup.py
+++ api/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='geocodr',
     description="Geocoder API for Solr",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
