7975278c6a9c4741c4f68ca17c0d6c8f8377cc8d
Oliver Tonnhofer <olt@bogosoft.com>
2019-02-26 08:28:38 +0100

    bump versions

--- import/setup.py
+++ import/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='geocodr_import',
     description="Import tools for geocodr",
-    version="0.1",
+    version="0.3.0",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
